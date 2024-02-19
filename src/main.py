7b666dd5556b4911ea9d385973372a15ca1c5dd9
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-02-19 09:23:18 -0300

    Secret bot token is now read from a local environment variable.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from dotenv import load_dotenv
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="Mirá cambié el texto dinámicamente"
+        chat_id=update.effective_chat.id, text="Ya quité el token del código 2.0!"
     )
 
 
 if __name__ == "__main__":
-    bot_token = "6454892729:AAExNn5VsvyXAmua9s1D-fWy-YANvRgYqnE"
+    load_dotenv()
+    bot_token = os.environ["BOT_TOKEN"]
     application = ApplicationBuilder().token(bot_token).build()
 
     start_handler = CommandHandler("start", start)
