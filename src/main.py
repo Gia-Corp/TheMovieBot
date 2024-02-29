f4cd38bb81e53cb4bccce9e218c3af0b01b28c7e
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-02-29 14:16:38 +0000

    Added /getmovie command. Created menu for listing all available bot commands.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,34 @@
 import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import BotCommand, Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, Application
 from dotenv import load_dotenv
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Ahora te contesto esto")
+    await update.message.reply_text("Hola!")
+
+
+async def get_movie(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Acá va a aparecer un menú")
+
+
+async def post_init(application: Application):
+    command_info = [
+        BotCommand("start", "Saludá al bot"),
+        BotCommand("getmovie", "Buscá una película por su nombre"),
+    ]
+    await application.bot.set_my_commands(commands=command_info)
 
 
 if __name__ == "__main__":
     load_dotenv()
     bot_token = os.environ["BOT_TOKEN"]
-    application = ApplicationBuilder().token(bot_token).build()
+    application = ApplicationBuilder().token(bot_token).post_init(post_init).build()
 
     start_handler = CommandHandler("start", start)
     application.add_handler(start_handler)
+    get_movie_handler = CommandHandler("getmovie", get_movie)
+    application.add_handler(get_movie_handler)
 
     print("🟢 Bot started successfully!")
 
