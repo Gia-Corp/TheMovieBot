447a78958f0324e9e2820e450294b6e2e78129d0
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-02-29 17:50:14 +0000

    Added conversation handler to retrieve movies by name.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 import os
 from telegram import BotCommand, Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, Application
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    Application,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 from dotenv import load_dotenv
 
 
@@ -9,7 +17,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def get_movie(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Acá va a aparecer un menú")
+    await update.message.reply_text("¿Qué peli estás buscando?")
+    return NAME
+
+
+async def movie_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("La peli que se busca es: " + update.message.text)
+    return ConversationHandler.END
 
 
 async def post_init(application: Application):
@@ -20,16 +34,36 @@ async def post_init(application: Application):
     await application.bot.set_my_commands(commands=command_info)
 
 
-if __name__ == "__main__":
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Ok, no querés seguir buscando...")
+    return ConversationHandler.END
+
+
+NAME = 0
+
+
+def main() -> None:
     load_dotenv()
     bot_token = os.environ["BOT_TOKEN"]
     application = ApplicationBuilder().token(bot_token).post_init(post_init).build()
 
     start_handler = CommandHandler("start", start)
+    cancel_handler = CommandHandler("cancel", cancel)
+
     application.add_handler(start_handler)
-    get_movie_handler = CommandHandler("getmovie", get_movie)
+    application.add_handler(cancel_handler)
+
+    get_movie_handler = ConversationHandler(
+        entry_points=[CommandHandler("getmovie", get_movie)],
+        states={NAME: [MessageHandler(filters.TEXT, movie_name)]},
+        fallbacks=[cancel_handler],
+    )
     application.add_handler(get_movie_handler)
 
     print("🟢 Bot started successfully!")
 
     application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
