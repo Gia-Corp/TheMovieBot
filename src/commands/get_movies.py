077d292d9e83f9c70fb0f26a9fa2350e982f3780
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-03-04 18:37:15 -0300

    chore: moved callback query data logging after answer method is called

--- src/commands/get_movies.py
+++ src/commands/get_movies.py
@@ -63,8 +63,8 @@ def build_keyboard(pagination_metadata) -> InlineKeyboardMarkup:
 
 async def handle_pagination(update, context):
     query = update.callback_query
-    print(f"callback_data recibido: {query.data}")
     await query.answer()
+    print(f"callback_data recibido: {query.data}")
     metadata, movies = await get_movies_from_api(query.data)
     text = create_reply_text(movies, metadata)
 
